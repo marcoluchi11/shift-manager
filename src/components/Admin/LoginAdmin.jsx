16fb99fad8b8a4fec1db6d6354a52a92338d23ed
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-22 10:40:06 -0300

    add login for admin complete

--- src/components/Admin/LoginAdmin.jsx
+++ src/components/Admin/LoginAdmin.jsx
@@ -1,62 +1,24 @@
-import styled from "styled-components";
+import { useContext } from "react";
+import { ShiftContext } from "../../context/ShiftContext";
 
-const Formulary = styled.form`
-  display: flex;
-  justify-content: center;
-  align-self: center;
-  flex-direction: column;
-  background-color: rgba(255, 255, 255, 0.13);
-  border-radius: 10px;
-  border: 2px solid rgba(255, 255, 255, 0.1);
-  box-shadow: 0 0 30px rgba(8, 7, 16, 0.6);
-  padding: 5rem;
-  div {
-    margin: 0.5rem;
-    display: flex;
-    flex-direction: column;
-  }
-  .email {
-    margin-top: 1rem;
-  }
-  .pass {
-    margin-top: 1rem;
-  }
-  input[type="password"],
-  input[type="email"] {
-    height: 1.5rem;
-    background-color: rgba(255, 255, 255, 0.07);
-    border-radius: 3px;
-    margin-top: 0.5rem;
-    padding: 0 0.5rem;
-
-    outline: 0;
-  }
-  input::placeholder {
-    font-weight: 100;
-  }
-  input[type="submit"] {
-    margin-top: 1rem;
-    width: 100%;
-    background-color: #ffffff;
-    color: #080710;
-    padding: 15px 0;
-    font-size: 18px;
-    font-weight: 600;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-`;
 const LoginAdmin = () => {
+  const { login, setLogin } = useContext(ShiftContext);
   return (
-    <Formulary>
+    <div>
+      <h1>Sign in Admin</h1>
       <div>
         <label className="email" htmlFor="username">
           Nombre de Usuario
         </label>
         <input
+          onChange={(e) =>
+            setLogin({ ...login, [e.target.name]: e.target.value })
+          }
           autoComplete="off"
+          name="email"
           id="username"
           type="email"
+          value={login.email}
           placeholder="Escribe tu email..."
         />
       </div>
@@ -65,14 +27,18 @@ const LoginAdmin = () => {
           Password
         </label>
         <input
+          onChange={(e) =>
+            setLogin({ ...login, [e.target.name]: e.target.value })
+          }
           autoComplete="off"
           id="pass"
+          name="password"
           type="password"
+          value={login.password}
           placeholder="Escribe tu contrasenia..."
         />
       </div>
-      <input type="submit" value="Ingresar" />
-    </Formulary>
+    </div>
   );
 };
 
