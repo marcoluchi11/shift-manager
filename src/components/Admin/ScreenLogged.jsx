c39ad0f80e16c1dbb13f71a4f53dd63eb4df7caa
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-22 19:11:07 -0300

    add links to different sections of admin part

--- src/components/Admin/ScreenLogged.jsx
+++ src/components/Admin/ScreenLogged.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 import { ShiftContext } from "../../context/ShiftContext";
 import auth from "../../firebaseConfig";
@@ -15,16 +15,8 @@ const Container = styled.div`
   border: 2px solid rgba(255, 255, 255, 0.1);
   box-shadow: 0 0 30px rgba(8, 7, 16, 0.6);
   padding: 5rem;
-  button {
-    margin-top: 1rem;
-    width: 100%;
-    background-color: #ffffff;
-    color: #080710;
-    padding: 15px 0;
-    font-size: 18px;
-    font-weight: 600;
-    border-radius: 5px;
-    cursor: pointer;
+  a {
+    text-decoration: none;
   }
   ul {
     display: flex;
@@ -33,6 +25,10 @@ const Container = styled.div`
     }
     list-style-type: none;
     li {
+      @media (max-width: 720px) {
+        margin: 1rem 0;
+      }
+      border-radius: 5px;
       margin: 0 1rem;
       cursor: pointer;
       background-color: #fff;
@@ -41,24 +37,56 @@ const Container = styled.div`
     }
   }
 `;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5rem;
+  h3 {
+    margin: 0 1rem;
+    padding: 0;
+  }
+  button {
+    padding: 0 0.5rem;
+    margin: 0;
+    background-color: #ffffff;
+    color: #080710;
+    border: 0;
+    font-weight: 600;
+    border-radius: 5px;
+    outline: 0;
+    cursor: pointer;
+  }
+`;
 const ScreenLogged = () => {
   const { user } = useContext(ShiftContext);
+  const [create, setCreate] = useState(false);
   const logout = async (e) => {
     e.preventDefault();
     await signOut(auth);
   };
+
   return (
     <Container>
-      <h1>Welcome, {user.email}</h1>
-      <button onClick={logout}>Cerrar Sesion</button>
+      <Header>
+        <h3>Welcome {user.email}</h3>
+        <button onClick={logout}>Cerrar Sesion</button>
+      </Header>
       <div>
-        <ul>
-          <Link to="/admin/createuser">
-            <li>Crear Usuario</li>
-          </Link>
-          <li>Agregar Pack de clases</li>
-          <li>Ver reservas del dia</li>
-        </ul>
+        {create ? (
+          <CreateUser setCreate={setCreate} />
+        ) : (
+          <ul>
+            <Link to="/admin/createuser">
+              <li>Crear Usuario</li>
+            </Link>
+            <Link to="/admin/addpacks">
+              <li>Agregar Pack de clases</li>
+            </Link>
+            <Link to="/admin/reserves">
+              <li>Ver reservas del dia</li>
+            </Link>
+          </ul>
+        )}
       </div>
     </Container>
   );
