938340b6aa976e73033ea5819bb0c550c3314ff8
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-22 10:39:30 -0300

    add state for login and creation

--- src/context/ShiftContext.js
+++ src/context/ShiftContext.js
@@ -3,9 +3,15 @@ import { createContext, useState } from "react";
 export const ShiftContext = createContext();
 
 const ShiftProvider = ({ children }) => {
-  const [data, setData] = useState(0);
+  const [register, setRegister] = useState({ email: "", password: "" });
+  const [user, setUser] = useState(false);
+  const [login, setLogin] = useState({ email: "", password: "" });
   return (
-    <ShiftContext.Provider value={{ data }}>{children}</ShiftContext.Provider>
+    <ShiftContext.Provider
+      value={{ register, setRegister, login, setLogin, user, setUser }}
+    >
+      {children}
+    </ShiftContext.Provider>
   );
 };
 
