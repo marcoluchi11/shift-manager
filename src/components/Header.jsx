cd5b9622d0a22c276a6090924a4b4ca7624923f6
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-23 18:19:55 -0300

    add text-align center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  text-align: center;
+`;
 const Header = () => {
   return (
-    <header>
+    <Container>
       <h1>Gestion de Turnos</h1>
-    </header>
+    </Container>
   );
 };
 
