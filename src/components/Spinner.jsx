cbfc0621933ce951d473af572c9249cde4ca47c2
marcoluchi11 <marcoaluchi@hotmail.com>
2022-06-28 22:57:51 -0300

    add classname

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,13 +1,13 @@
 import "./spinner.css";
 const Spinner = () => {
   return (
-    <div class="sk-chase">
-      <div class="sk-chase-dot"></div>
-      <div class="sk-chase-dot"></div>
-      <div class="sk-chase-dot"></div>
-      <div class="sk-chase-dot"></div>
-      <div class="sk-chase-dot"></div>
-      <div class="sk-chase-dot"></div>
+    <div className="sk-chase">
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
     </div>
   );
 };
