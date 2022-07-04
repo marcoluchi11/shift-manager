3ed6c8fb5f2ba54d74c99ec048c3fd1f1866eef3
marcoluchi11 <marcoaluchi@hotmail.com>
2022-07-04 18:30:11 -0300

    add functions to use in components

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -1,6 +1,22 @@
-export function formatearDinero(dinero) {
+export function formatMoney(dinero) {
   return dinero.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
+}
+export function pricePerMonth(times) {
+  switch (times) {
+    case 2:
+      return 4000;
+    case 3:
+      return 4500;
+    case 4:
+      return 5000;
+    case 5:
+      return 5200;
+    case 6:
+      return 5500;
+    default:
+      return;
+  }
 }
