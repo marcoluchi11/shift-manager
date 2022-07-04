0439ae30090258385ec96616e6d5aa75fe1e704f
marcoluchi11 <marcoaluchi@hotmail.com>
2022-07-04 18:34:19 -0300

    update users state

--- src/components/User/WelcomeUser.jsx
+++ src/components/User/WelcomeUser.jsx
@@ -1,10 +1,22 @@
-import { useContext } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { useContext, useEffect } from "react";
 import { ShiftContext } from "../../context/ShiftContext";
+import { db } from "../../firebaseConfig";
 import { Container } from "../Admin/ScreenLogged";
 import Sections from "./Sections";
 
 const WelcomeUser = () => {
-  const { user } = useContext(ShiftContext);
+  const { user, setUsers } = useContext(ShiftContext);
+  const clientsCollectionRef = collection(db, "clients");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(clientsCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+    // eslint-disable-next-line
+  }, []);
   return (
     <Container>
       <h2>Hola {user.email}, Bienvenido a DF</h2>
