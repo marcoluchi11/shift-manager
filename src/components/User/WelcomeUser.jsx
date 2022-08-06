26365dd03d8faaa047eefa06fa28fad5780b6b91
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-06 14:20:10 -0300

    delete repeated code and redifined variable

--- src/components/User/WelcomeUser.jsx
+++ src/components/User/WelcomeUser.jsx
@@ -1,19 +1,12 @@
-import { collection, getDocs } from "firebase/firestore";
 import { useContext, useEffect } from "react";
 import { ShiftContext } from "../../context/ShiftContext";
-import { db } from "../../firebaseConfig";
 import { Container } from "../Admin/ScreenLogged";
 import Sections from "./Sections";
 
-const WelcomeUser = () => {
-  const { user, setUsers } = useContext(ShiftContext);
-  const clientsCollectionRef = collection(db, "clients");
+const WelcomeUser = ({ getUsers }) => {
+  const { user } = useContext(ShiftContext);
 
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(clientsCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
     getUsers();
     // eslint-disable-next-line
   }, []);
