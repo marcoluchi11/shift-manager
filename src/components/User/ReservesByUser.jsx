eb2bc6524aebacbd8757d8b2c341009379a0820b
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-09 12:31:23 -0300

    add function to translate month and put colon in the time

--- src/components/User/ReservesByUser.jsx
+++ src/components/User/ReservesByUser.jsx
@@ -1,11 +1,16 @@
 import { nanoid } from "nanoid";
+import { setColon, translateMonth } from "../helpers";
 
 const ReservesByUser = ({ reservesList }) => {
-  return reservesList.map((elem) => (
-    <p key={nanoid()}>
-      {elem.day} at {elem.time}
-    </p>
-  ));
+  return reservesList.map((elem) => {
+    const mes = elem.day.split(" ");
+
+    return (
+      <p key={nanoid()}>
+        {mes[0]} de {translateMonth(mes[1])} a las {setColon(elem.time)}hs.
+      </p>
+    );
+  });
 };
 
 export default ReservesByUser;
