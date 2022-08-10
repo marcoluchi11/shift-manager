b9b58e497a7bec45e3c50627d391f4aeb298d190
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-10 16:59:26 -0300

    add feature that doesn't show previous date

--- src/components/User/ReservesByUser.jsx
+++ src/components/User/ReservesByUser.jsx
@@ -4,7 +4,9 @@ import { setColon, translateMonth } from "../helpers";
 const ReservesByUser = ({ reservesList }) => {
   return reservesList.map((elem) => {
     const mes = elem.day.split(" ");
-
+    const today = new Date();
+    if (today.getDate() > mes[0]) return null;
+    //RESOLVER PROBLEMA CON EL MES
     return (
       <p key={nanoid()}>
         {mes[0]} de {translateMonth(mes[1])} a las {setColon(elem.time)}hs.
