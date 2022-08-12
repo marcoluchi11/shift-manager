addeb836e5a2d54aeb3b25c88eeabe88fcabb272
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-12 17:48:17 -0300

    add container and send updatedata function to cancelReserve component

--- src/components/User/ReservesByUser.jsx
+++ src/components/User/ReservesByUser.jsx
@@ -1,16 +1,27 @@
 import { nanoid } from "nanoid";
+import styled from "styled-components";
 import { setColon, translateMonth } from "../helpers";
-
-const ReservesByUser = ({ reservesList }) => {
+import CancelReserve from "./CancelReserve";
+const Container = styled.div`
+  display: flex;
+`;
+const ReservesByUser = ({ reservesList, updateData }) => {
   return reservesList.map((elem) => {
     const mes = elem.day.split(" ");
     const today = new Date();
     if (today.getDate() > mes[0]) return null;
     //RESOLVER PROBLEMA CON EL MES
     return (
-      <p key={nanoid()}>
-        {mes[0]} de {translateMonth(mes[1])} a las {setColon(elem.time)}hs.
-      </p>
+      <Container key={nanoid()}>
+        <p>
+          {mes[0]} de {translateMonth(mes[1])} a las {setColon(elem.time)}hs.
+        </p>
+        <CancelReserve
+          reservesList={reservesList}
+          target={elem}
+          updateData={updateData}
+        />
+      </Container>
     );
   });
 };
